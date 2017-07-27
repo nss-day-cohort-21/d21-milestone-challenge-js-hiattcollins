6aecc9249c2c434ee5d401ccf379b2ce03d29976
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-07-27 11:27:57 -0500

    cleaned up JS and added comments

--- main.js
+++ main.js
@@ -1,29 +1,18 @@
-console.log("main.js");
+// console.log("main.js");
 
-// 
 
-// treeObject.num = document.getElementById("treeHeight").value;
-// treeObject.char = document.getElementById("treeChar").value;
-
-// console.log("treeObject.num", treeObject.num);
-// console.log("treeObject.char", treeObject.char);
+// ***** Function to check whether inputs are correct ***** //
 
 function checkInputs() {
 
 	theNum = document.getElementById("treeHeight").value;
 	theChar = document.getElementById("treeChar").value;
 
-	console.log("theNum:", theNum, "theChar:", theChar);
-
 	if (isNaN(theNum)) {
 
 		alert("Please enter a number in the first box!");
 
 	} else if (theNum && theChar ) {
-
-	// console.log("Nothing is in treeObject.num", treeObject.num);
-	// alert("Please enter both a number and a character!");
-	// alert("theNum and theChar both had a character");
 
 		buildArray(theNum, theChar);
 
@@ -33,68 +22,36 @@ function checkInputs() {
 	}
 }
 
+
+// ***** Function to build array to hold input number and character ***** //
+
 function buildArray(theNum, theChar) {
 
 	let treeObject = {};
 
-// treeObject.num = parseInt(document.getElementById("treeHeight").value);
-// treeObject.char = document.getElementById("treeChar").value;
-
 	treeObject.num = parseInt(theNum);
 	treeObject.char = theChar;
 
-
-	console.log("treeObject.num", treeObject.num);
-	console.log("treeObject.char", treeObject.char);
-	console.log("treeObject", treeObject);
-
-	console.log(4 + treeObject.num);
-
-// if (treeObject.num) {
-// 	console.log("treeObject.num has a value:", treeObject.num);
-// } else {
-// console.log("treeObject.num has no value:", treeObject.num);
-// // }
-
-// if (treeObject.num || treeObject.char) {
-// 	console.log("either treeObject.num or treeObject.char has a value");
-// 	console.log("treeObject.num", treeObject.num);
-// 	console.log("treeObject.char", treeObject.char);
-// } else {
-// 	console.log("either treeObject.num or treeObject.char has NO value");
-// 	console.log("treeObject.num", treeObject.num);
-// 	console.log("treeObject.char", treeObject.char);
-// }
-
-// event.preventDefault();
-
-tree(treeObject); 
-
-
-
-
+	tree(treeObject); 
 }
 
 
-function tree(input) {
-	console.log("input.num", input.num);
-	console.log("input.char", input.char);
-	console.log("input", input);
+// ***** Function to build tree in console ***** //
 
-	console.log(4 + input.num);
+function tree(input) {
 
 	let numberSpacers = "";
 	let spacerCounter = input.num;
 
-	let numberStars = input.char;
-	let starCounter = 0;
+	let numberChars = input.char;
+	let charCounter = 0;
 
 	let theString = "";
 
 	for (var i = 0; i < input.num; i++) {
 		
 		numberSpacers = "";
-		numberStars = input.char;
+		numberChars = input.char;
 
 		spacerCounter -= 1;
 		
@@ -102,36 +59,27 @@ function tree(input) {
 			numberSpacers += " ";
 		}
 
-		for (var k = 0; k < starCounter; k++) {
-			numberStars += input.char + input.char;
+		for (var k = 0; k < charCounter; k++) {
+			numberChars += input.char + input.char;
 		}
 		
-		starCounter += 1;
+		charCounter += 1;
 
-		theString = numberSpacers + numberStars + numberSpacers;
+		theString = numberSpacers + numberChars + numberSpacers;
 
 		console.log(theString);
-				// console.log("numberSpacers", numberSpacers);
-		
-			
 
-		// theString = numberSpacers + input.char + numberSpacers;
-
-		// console.log("theString", theString);
-		// console.log(theString);
 	}
-
-	// event.preventDefault();	
 }
 
 
-// ***** Add event listener to tree button on HTML page and trigger tree grow on click ***** //
-// document.getElementById("growTree").addEventListener("click", buildArray);
+// ***** Add event listener to tree button on HTML page and trigger functions on click ***** //
 
 document.getElementById("growTree").addEventListener("click", checkInputs);
 
 
-// ***** Set up enter key to activate tree grow function ***** //
+// ***** Set up enter key to activate functions ***** //
+
 document.addEventListener("keypress", function(event) {
   if (event.keyCode === 13) {
     checkInputs();
